c4f876739e9d8cdb58a0c5cc7b313f9a27247145
Macr0phag3 <997334636@qq.com>
2018-11-14 17:35:10 +0800

    upload latest code

--- test.py
+++ test.py
@@ -1,69 +1,69 @@
 # -*- coding: utf-8 -*-
 
-from time import sleep
-import random
+import curses
+import time
 import threading
-import argparse
-import signal
-import sys
-import os
-import do_not_use_it
-
-parser = argparse.ArgumentParser()
-parser.add_argument(u"-faddr", u"--from_address",
-                    help=u"fake-from-address", required=True)
-
-parser.add_argument(u"-taddr", u"--to_address",
-                    help=u"the address you want to delivery", required=True)
-
-parser.add_argument(u"-s", u"--subject",
-                    help=u"email's subject", required=True)
-
-parser.add_argument(u"-b", u"--body",
-                    help=u"email's body(content)", required=True)
-
-parser.add_argument(u"-tnum", u"--threads_num",
-                    help=u"how many threads you want (default is 1)", default=1, type=int)
-
-parser.add_argument(u"-v", u"--verbose",
-                    help=u"verbose level (choice in [0, 1, 2, 3])", default=-1, type=int)
-
-parser.add_argument(u"-c", u"--crazy_mode",
-                    help=u"Keep sending fake-email (default is False ** Use with caution **)", action='store_true', default=False)
 
 
-args = parser.parse_args()
+class Pad:
+    def __init__(self, length, width):
+        self.length = length  # 长度
+        self.width = width  # 宽度
 
-succ_num = failed_num = 0
-quit_flag = print_flag = 1
-ver = -1
-from_addr = args.from_address
-to_addr = args.to_address
-subject = args.subject.decode("utf8") if vars(str).get("decode") else args.subject
-body = (args.body.decode(
-    "utf8") if vars(str).get("decode") else args.body).replace("\\n", "\r\n")
-threads_num = args.threads_num if args.threads_num > 0 else 1
-verbose = args.verbose
-crazy_mode = args.crazy_mode
-threads_alive = [1]*threads_num
-Data = [do_not_use_it.PutColor('0', "cyan")]*threads_num
-Lock = threading.Lock()
+        self.width_show = self.width/3  # 展现的宽度
 
-do_not_use_it.ShowLogo()
+        self.x = 0
+        self.y = 0  # lake max y :)
 
-ea = do_not_use_it.SpamEmail(to_addr, from_addr)
+        curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+        self.initscr()
+
+    def initscr(self):
+
+        self.the_pad = curses.newpad(self.length, self.width)
+
+        self.the_pad.keypad(1)
+
+        show_thread = threading.Thread(target=self.show)
+        show_thread.start()
+
+    def refresh(self):
+        self.the_pad.refresh(self.x, self.y, 0, 0, self.width_show, self.length)
+
+    def draw(self, x, y, string):
+        try:
+            self.the_pad.addstr(x, y, string)
+        except curses.error:
+            return False
+
+        self.refresh()
+
+        """
+        if x > self.width_show:
+            self.x += 1
+        """
+
+    def show(self):
+        while 1:
+            cmd = self.the_pad.getch()
+            if cmd == curses.KEY_DOWN:
+                if self.x < self.width:
+                    self.x += 1
+            elif cmd == curses.KEY_UP:
+                if self.x > 0:
+                    self.x -= 1
+            else:
+                break
+
+            self.refresh()
+
+        curses.endwin()
 
 
-ea.DNSQuery()
-print u"connect to %s:%s " % (ea.SMTP_addr, str(ea.port))
-print ea.Connect()
+pad = Pad(100, 50)
 
-print ea.Send("ehlo anti-anti-spam")
-status, data = ea.Recv()
-if not status:
-    sys.exit("Over")
-else:
-    for d in data:
-        print d
-
-print "\033[?25h"
+for i in range(200):
+    pad.draw(i, 0, str(i))
+    time.sleep(0.1)
