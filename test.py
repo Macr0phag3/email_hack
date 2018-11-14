0db3d3d0e86f12fa26d8243d610d19a02f856c85
Macr0phag3 <997334636@qq.com>
2018-11-14 22:10:35 +0800

    upload latest code

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ class Pad:
         self.length = length  # 长度
         self.width = width  # 宽度
 
+        self.length_show = self.length  # 展现的长度
         self.width_show = self.width/3  # 展现的宽度
 
         self.x = 0
@@ -18,11 +19,11 @@ class Pad:
         curses.initscr()
         curses.noecho()
         curses.cbreak()
-        self.initscr()
+        self.init_pad()
 
-    def initscr(self):
-
-        self.the_pad = curses.newpad(self.length, self.width)
+    def init_pad(self):
+        self.the_pad = curses.newwin(self.length, self.width)
+        #self.the_pad = curses.newpad(self.length, self.width)
 
         self.the_pad.keypad(1)
 
@@ -30,9 +31,36 @@ class Pad:
         show_thread.start()
 
     def refresh(self):
-        self.the_pad.refresh(self.x, self.y, 0, 0, self.width_show, self.length)
+        # self.x: 矩形左上角的 x 坐标（即 @ 的 x 坐标）
+        # self.y: 矩形左上角的 y 坐标（即 @ 的 y 坐标）
+        # 0: 起始的 x 坐标
+        # 0: 起始的 y 坐标
+        # self.width_show: 结束的 x 坐标
+        # self.length: 结束的 y 坐标
+        #
+        # 示例
+        # **@@@@@******
+        # **@@@@@******
+        # **@@@@@******
+        # *************
+        #
+        # 左上角的 @ 所在的坐标为：self.x, self.y，这里即为 (2, 0)
+        # 如果想刷新上面所有的 @，则后面 4 个参数应写为：()
+        self.the_pad.refresh()  # self.x, self.y, 0, 0, self.width, self.length)
 
     def draw(self, x, y, string):
+        """
+        print len(string)
+        self.length = 10000
+        self.the_pad.resize(self.length, self.width)
+        self.refresh()
+        """
+
+        if len(string) > self.length:
+            self.length = len(string)
+            self.the_pad.resize(self.length, self.width)
+            self.refresh()
+
         try:
             self.the_pad.addstr(x, y, string)
         except curses.error:
@@ -62,8 +90,9 @@ class Pad:
         curses.endwin()
 
 
-pad = Pad(100, 50)
+pad = Pad(100, 10)
 
 for i in range(200):
-    pad.draw(i, 0, str(i))
-    time.sleep(0.1)
+    pad.draw(i, 0, str(i)*100)
+    time.sleep(0.5)
+    break
