7124115ad766bf23c3249411e0c95e3ff152949b
Macr0phag3 <997334636@qq.com>
2018-11-14 23:43:32 +0800

    upload latest code

--- test1.py
+++ test1.py
@@ -25,7 +25,7 @@ class Screen(object):
             ┌--------------------------------------┐
             |1. Item                               |
             |--------------------------------------| <- top = 1
-            |2. Item                               | 
+            |2. Item                               |
             |3. Item                               |
             |4./Item///////////////////////////////| <- current = 3
             |5. Item                               |
@@ -42,20 +42,14 @@ class Screen(object):
         Returns
             None
         """
-        self.window = None
-
-        self.width = 0
-        self.height = 0
 
         self.init_curses()
 
-        self.items = items
-
-        self.max_lines = curses.LINES
         self.top = 0
+        self.items = items
         self.bottom = len(self.items)
-        self.current = 0
-        self.page = self.bottom // self.max_lines
+        self.current = self.bottom - 1
+        self.max_lines = curses.LINES
 
         self.run()
 
@@ -67,16 +61,19 @@ class Screen(object):
         curses.noecho()
         curses.cbreak()
 
+        """
         curses.start_color()
         curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
         curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_CYAN)
 
         self.current = curses.color_pair(2)
+        """
 
         self.height, self.width = self.window.getmaxyx()
 
     def run(self):
         """Continue running the TUI until get interrupted"""
+
         try:
             self.input_stream()
         except KeyboardInterrupt:
@@ -94,77 +91,36 @@ class Screen(object):
                 self.scroll(self.UP)
             elif ch == curses.KEY_DOWN:
                 self.scroll(self.DOWN)
-            elif ch == curses.KEY_LEFT:
-                self.paging(self.UP)
-            elif ch == curses.KEY_RIGHT:
-                self.paging(self.DOWN)
             elif ch == curses.ascii.ESC:
                 break
-
-    def scroll(self, direction):
-        """Scrolling the window when pressing up/down arrow keys"""
-        # next cursor position after scrolling
-        next_line = self.current + direction
-
-        # Up direction scroll overflow
-        # current cursor position is 0, but top position is greater than 0
-        if (direction == self.UP) and (self.top > 0 and self.current == 0):
-            self.top += direction
-            return
-        # Down direction scroll overflow
-        # next cursor position touch the max lines, but absolute position of max lines could not touch the bottom
-        if (direction == self.DOWN) and (next_line == self.max_lines) and (self.top + self.max_lines < self.bottom):
-            self.top += direction
-            return
-        # Scroll up
-        # current cursor position or top position is greater than 0
-        if (direction == self.UP) and (self.top > 0 or self.current > 0):
-            self.current = next_line
-            return
-        # Scroll down
-        # next cursor position is above max lines, and absolute position of next cursor could not touch the bottom
-        if (direction == self.DOWN) and (next_line < self.max_lines) and (self.top + next_line < self.bottom):
-            self.current = next_line
-            return
-
-    def paging(self, direction):
-        """Paging the window when pressing left/right arrow keys"""
-        current_page = (self.top + self.current) // self.max_lines
-        next_page = current_page + direction
-        # The last page may have fewer items than max lines,
-        # so we should adjust the current cursor position as maximum item count on last page
-        if next_page == self.page:
-            self.current = min(self.current, self.bottom % self.max_lines - 1)
-
-        # Page up
-        # if current page is not a first page, page up is possible
-        # top position can not be negative, so if top position is going to be negative, we should set it as 0
-        if (direction == self.UP) and (current_page > 0):
-            self.top = max(0, self.top - self.max_lines)
-            return
-        # Page down
-        # if current page is not a last page, page down is possible
-        if (direction == self.DOWN) and (current_page < self.page):
-            self.top += self.max_lines
-            return
 
     def display(self):
         """Display the items on window"""
         self.window.erase()
+        #self.current = 0
         for idx, item in enumerate(self.items[self.top:self.top + self.max_lines]):
             # Highlight the current cursor line
             if idx == self.current:
-                self.window.addstr(idx, 0, item, curses.color_pair(2))
+                self.window.addstr(idx, 0, item+"<=%d" % self.current)  # , curses.color_pair(2))
             else:
-                self.window.addstr(idx, 0, item, curses.color_pair(1))
+                self.window.addstr(idx, 0, item)  # , curses.color_pair(1))
+
         self.window.refresh()
 
+    def scroll(self, direction):
+        """Scrolling the window when pressing up/down arrow keys"""
 
-def main():
-    items = [f'{num + 1}. Item' for num in range(1000)]
-    Screen(items)
+        # Up direction scroll overflow
+        # current cursor position is 0, but top position is greater than 0
+        if (direction == self.UP) and (self.top > 0):
+            self.top += direction
+            return
+        # Down direction scroll overflow
+        # next cursor position touch the max lines, but absolute position of max lines could not touch the bottom
+        if (direction == self.DOWN) and (self.top + self.max_lines < self.bottom):
+            self.top += direction
+            return
 
 
-if __name__ == '__main__':
-    main()
-
+items = ['{}. Item'.format(num) for num in range(30)]
+Screen(items)
