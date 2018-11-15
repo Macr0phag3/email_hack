0506492136ec9d2c0de399f610e5a00eaa4e720a
Macr0phag3 <997334636@qq.com>
2018-11-15 16:43:56 +0800

    upload latest code

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import curses
 import os
 import threading
 import time
+import random
 
 
 class Screen:
@@ -25,10 +26,11 @@ class Screen:
         self.init_curses()
 
         self.top = 0
-        self.items = items
+        self.items = ITEMS
         self.bottom = len(self.items)
         self.current = self.bottom - 1
         self.max_lines = curses.LINES
+
         self.hori_len = 0
 
         self.run()
@@ -38,7 +40,7 @@ class Screen:
         self.window = curses.initscr()
         self.height, self.width = self.window.getmaxyx()
         self.window.keypad(True)
-
+        self.window.nodelay(True)
         curses.noecho()
         curses.cbreak()
 
@@ -49,17 +51,6 @@ class Screen:
             return -1
 
         return curses.color_pair(1) | curses.A_RIGHT
-
-    def run(self):
-        """Continue running the TUI until get interrupted"""
-
-        try:
-            self.input_stream()
-        except KeyboardInterrupt:
-            pass
-
-        os.system("printf '\e]50;ClearScrollback\a'")  # 兼容 iTerm2
-        curses.endwin()
 
     def input_stream(self):
         """Waiting an input and run a proper method according to type of input"""
@@ -85,11 +76,11 @@ class Screen:
                 break
 
     def display(self):
-        """Display the items on window"""
+        """Display the ITEMS on window"""
         self.window.erase()
 
         for idx, item in enumerate(self.items[self.top:self.top + self.max_lines]):
-            data = item
+            data = item.data
 
             # 1. 字符串长度超出默认 win 的长度，需要扩大 win
             # 2. 终端宽度被调整，需要 resize
@@ -117,34 +108,73 @@ class Screen:
             elif (direction == self.DOWN) and (self.top + self.max_lines < self.bottom):
                 self.top += direction
 
+    def run(self):
+        """Continue running the TUI until get interrupted"""
+        global EXIT_FLAG
 
-def create_screen():
-    Screen()
+        try:
+            self.input_stream()
+        except KeyboardInterrupt:
+            pass
+
+        os.system("printf '\e]50;ClearScrollback\a'")  # 兼容 iTerm2
+        curses.endwin()
+
+
+class Line:
+    '''
+    每一行为一个实例
+    '''
+
+    def __init__(self, id, data=""):
+        self.header = "{0:{fill}{align}{length}}".format(
+            "No."+str(id)+": ", length=len(str(THREADS_NUM))+5, fill=" ", align=">"
+        )  # 不变的数据头。使用空格进行右对齐
+
+        self.body = data  # 数据体，具体数据
+        self.data = self.header+self.body  # 真正展示出来的数据为 数据头+数据体
+
+    def update_body(self, data):
+        '''
+        更新数据体的时候也得更新真正展示出来的数据
+        '''
+
+        self.body = data
+        self.data = self.header+self.body
 
 
 def process_data():
-    global items
+    '''
+    处理数据
+    这里更新的数据会在线程 thread_screen 中展示出来
+    '''
 
-    for i in range(10):
-        items[i] += "1"
-        time.sleep(1)
+    while EXIT_FLAG:
+        ITEMS[random.randint(0, THREADS_NUM-1)].update_body("attacking...")
+        time.sleep(0.5)
 
 
-threads_num = 20
+# ---------- 全局变量 -----------
+EXIT_FLAG = 1
 
-items = ['']*threads_num
+THREADS_NUM = 20
 
-"""
-items = ["{0:{fill}{align}{length}}".format(
-    "No."+str(num)+": ", length=len(str(threads_num))+5, fill=" ", align=">"
-) for num in range(threads_num)]
-"""
+# 每一行的数据
+ITEMS = [Line(num, "starting...") for num in range(THREADS_NUM)]
+
+# ------------------------------
+
 
 thread_screen = threading.Thread(target=Screen)
-thread_screen.start()
+thread_screen.start()  # 启动线程 thread_screen 用于展示数据
 
 thread_data = threading.Thread(target=process_data)
-thread_data.start()
-thread_data.join()
+thread_data.start()  # 启动线程 thread_data 用于处理数据
+
+thread_screen.join()  # 阻塞是必须的
+
+
+# ------------- 准备退出 -----------------
+EXIT_FLAG = 0  # screen 的线程 gg 的时候，退出 process_data
 
 print "Bye~"
